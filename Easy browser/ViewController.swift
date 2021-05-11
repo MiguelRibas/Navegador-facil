6523085b73b25acdcd7b20c1470943eda548e173
MiguelRibas <54724261+MiguelRibas@users.noreply.github.com>
2021-05-11 16:41:59 -0300

    Refatorando

--- Easy browser/ViewController.swift
+++ Easy browser/ViewController.swift
@@ -10,6 +10,7 @@ import WebKit
 
 class ViewController: UIViewController, WKNavigationDelegate {
 
+    var websites = ["apple.com", "hackingwithswift.com"]
     var webView: WKWebView!
     
     override func loadView() {
@@ -21,7 +22,7 @@ class ViewController: UIViewController, WKNavigationDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        guard let url = URL(string: "https://www.hackingwithswift.com") else { return }
+        guard let url = URL(string: "https://" + websites[0]) else { return }
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
         
@@ -30,8 +31,9 @@ class ViewController: UIViewController, WKNavigationDelegate {
     
     @objc func openTapped() {
         let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
-        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
-        ac.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage))
+        for website in websites {
+            ac.addAction(UIAlertAction(title: website, style: .default, handler: openPage))
+        }
         ac.addAction(UIAlertAction(title: "cancel", style: .cancel))
         ac.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
         present(ac, animated: true)
@@ -40,10 +42,20 @@ class ViewController: UIViewController, WKNavigationDelegate {
     func openPage(action: UIAlertAction) {
         let url = URL(string: "https://" + action.title!)!
         webView.load(URLRequest(url: url))
-        
-        func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-            title = webView.title
+    }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let url = navigationAction.request.url
+
+        if let host = url?.host {
+            for website in websites {
+                if host.contains(website) {
+                    decisionHandler(.allow)
+                    return
+                }
+            }
         }
+        decisionHandler(.cancel)
     }
 }
 
