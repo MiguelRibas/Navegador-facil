9328714ceb8d4c7189a83c9184bbebd4b0b3e7d1
MiguelRibas <54724261+MiguelRibas@users.noreply.github.com>
2021-05-12 11:40:54 -0300

    Ultimos ajustes

--- Easy browser/ViewController.swift
+++ Easy browser/ViewController.swift
@@ -9,50 +9,44 @@ import UIKit
 import WebKit
 
 class ViewController: UIViewController, WKNavigationDelegate {
-    
+        
     // MARK: - VARIABLES -
-    var websites = ["apple.com", "hackingwithswift.com"]
+    var websites = ["apple.com","google.com"]
     var webView: WKWebView!
     
     //MARK: - LIFE CYCLE -
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.getService()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open",
+                                                            style: .plain,
+                                                            target: self,
+                                                            action: #selector(openTapped))
+    }
+    
     override func loadView() {
         webView = WKWebView()
         webView.navigationDelegate = self
         view = webView
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.getService()
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
-    }
-    
     // MARK: - IBACTIONS -
     @objc func openTapped() {
-        let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
+        let ac = UIAlertController(title: "Open page...",
+                                   message: nil,
+                                   preferredStyle: .actionSheet)
         for website in websites {
-            ac.addAction(UIAlertAction(title: website, style: .default, handler: openPage))
-        }
-        ac.addAction(UIAlertAction(title: "cancel", style: .cancel))
+            ac.addAction(UIAlertAction(title: website,
+                                       style: .default,
+                                       handler: openPage))}
+        ac.addAction(UIAlertAction(title: "cancel",
+                                   style: .cancel))
         ac.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
         present(ac, animated: true)
     }
-    
-    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
-        let url = navigationAction.request.url
-
-        if let host = url?.host {
-            for website in websites {
-                if host.contains(website) {
-                    decisionHandler(.allow)
-                    return
-                }
-            }
-        }
-        decisionHandler(.cancel)
-    }
 }
 
+// MARK: - AUX FUNC -
 extension ViewController {
     func getService() {
         guard let url = URL(string: "https://" + websites[0]) else { return }
@@ -64,5 +58,17 @@ extension ViewController {
         let url = URL(string: "https://" + action.title!)!
         webView.load(URLRequest(url: url))
     }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let url = navigationAction.request.url
+        if let host = url?.host {
+            for website in websites {
+                if host.contains(website) {
+                    decisionHandler(.allow)
+                    return
+                }
+            }
+        }
+        decisionHandler(.cancel)
+    }
 }
-
